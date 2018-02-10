e6a775da25c350a0e0e0dbb778ad4d692c9c2579
josechoy <19215600+josechoy@users.noreply.github.com>
2018-02-09 22:45:30 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # simple script to deploy your node app to heroku server
-# make sure git add and git commit any changes before run this script
+# make sure git add and git commit any changes 
+#   to your local masterbefore run this script
 # usage:
 # ./deploy.sh <APP_NAME>
 
